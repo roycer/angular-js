5104fc22d1ec7e26911c6a35a6babb0d16674b54
jermirm <jermi.rm.work@gmail.com>
2017-06-07 21:07:21 -0500

    login

---
+++ controllerjs/controllers/loginController.js
@@ -0,0 +1,18 @@
+angular.module('sigbook', ['loginService']);
+
+angular.module('sigbook').controller('loginController', ['$scope', '$window', 'loginRequest', loginController]);
+function loginController($scope, $window, loginRequest){
+	
+	$scope.user = {};
+	
+	$scope.checkLogin = function(){
+		console.log('CheckLogin');
+		
+		loginRequest.checkLogin($scope.user).success(function(data){
+						
+			if (data){
+				$window.location.href = 'main.html';
+			}
+		});
+	};
+}
